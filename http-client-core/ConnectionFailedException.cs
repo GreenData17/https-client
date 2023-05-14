dd020492d961a2a1bd0ce1e54713c3deb25fb28c
GreenData17 <e.eggenberger@quickline.com>
2023-05-15 01:23:30 +0200

    A Lot

---
+++ http-client-core/ConnectionFailedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace http_client_core
+{
+    public class ConnectionFailedException : Exception
+    {
+        public ConnectionFailedException(string message) 
+            :base("ERROR: " + message) 
+        { }
+    }
+}
